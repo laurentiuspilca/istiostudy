785d6717d87665bc91339f7665e7fbd5377942f9
Laurentiu Spilca <laurentiu.spilca@endava.com>
2019-02-16 13:08:32 +0200

    Add project

---
+++ src/test/java/com/example/istio1/Istio1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.istio1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Istio1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
